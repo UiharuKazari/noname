8ff64d042387e87ec3dae7625c5d46af13a210ee
UiharuKazari <mascotzhang2015@gmail.com>
2017-09-14 02:51:59 +0000

    TIC-TAC-TOE 81%...

--- tic-tac-toe/main.cpp
+++ tic-tac-toe/main.cpp
@@ -3,7 +3,38 @@
 using namespace std;
 
 int main() {
-	ChessBoard cb;
+	int choice;
+	cout << "\t1. Tic-Tac-Toe" << endl	
+			<< "\t2. file-in-a-row" << endl
+			<< "\t3. custom" << endl
+			<< "Choose game type (1, 2, 3): ";
+	cin >> choice;
+
+	int length, width, win_num;
+	switch(choice) {
+		case 1:
+			length=4;
+			width=4;
+			win_num=3;
+			break;
+		case 2:
+			length=16;
+			width=16;
+			win_num=5;
+			break;
+		case 3:
+			cout << "Enter win_num: ";
+			cin >> win_num;
+			cout << "Enter length: ";
+			cin >> length;
+			cout << "Enter width: ";
+			cin >> width;
+			break;
+		default:
+			break;
+	}
+
+	ChessBoard cb(win_num, length, width);
 	cb.show();
 
 /*
